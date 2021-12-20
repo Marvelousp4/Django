3ee65984f93ef3db5cde7df0ad847383acb3f4a2
Marvelousp4 <573335988@qq.com>
2021-12-20 20:10:22 +0800

    create project structure

---
+++ scripts/compress_game_js.sh
@@ -0,0 +1,7 @@
+JS_PATH=/home/marvelousp4/acapp/game/static/js/
+JS_PATH_DIST=${JS_PATH}dist/
+JS_PATH_SRC=${JS_PATH}src/
+
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+
+
